728b2ea013f4e6f334ddaca23b606c972602e815
MaryIvanisovaa <ivanisovamaria07@gmail.com>
2023-11-03 17:51:56 +0200

    there is new commit. i did it because i read your message about using include.text, have.text and should be visible

--- cypress/e2e/2-locators-and-actions/first.cy.js
+++ cypress/e2e/2-locators-and-actions/first.cy.js
@@ -4,10 +4,10 @@ describe('Check text of "Step content#1"', ()=>{
 
 it ('Check text of "Step content#1', ()=>{
 cy.visit('pages/layout/stepper');
-cy.get('.selected').eq(0).should('contain', 'First step').get('[nbsteppernext=""]').eq(1).click()
-cy.get('.selected').eq(0).should('contain', 'Second step').get('[nbsteppernext=""]').eq(0).click()
-cy.get('.selected').eq(0).should('contain', 'Third step').get('[nbsteppernext=""]').eq(0).click()
-cy.get('.selected').eq(0).should('contain', 'Fourth step')
+cy.get('.selected').eq(0).should('include.text', 'First step').get('[nbsteppernext=""]').eq(1).click()
+cy.get('.selected').eq(0).should('include.text', 'Second step').get('[nbsteppernext=""]').eq(0).click()
+cy.get('.selected').eq(0).should('include.text', 'Third step').get('[nbsteppernext=""]').eq(0).click()
+cy.get('.selected').eq(0).should('include.text', 'Fourth step')
 
 });
 });
--- cypress/e2e/2-locators-and-actions/second.cy.js
+++ cypress/e2e/2-locators-and-actions/second.cy.js
@@ -4,9 +4,13 @@ describe('Check form "Enter name"', ()=>{
 it ('Check form "Enter name" on page modal-overlays/dialog(Return Result From Dialog)', ()=>{
 cy.visit('pages/modal-overlays/dialog');
 cy.get('.appearance-filled').last().click()
-cy.contains('Enter your name')
+cy.get('input').parent().should('be.visible')
+cy.contains('Enter your name').should('have.text', 'Enter your name')
 cy.get('input').last().get('.cancel').get('.status-success')
 
 });
 });
 });
+
+// тут є питання щодо 8 строчки. я спочатку написала просто cy.contains('Enter your name') і думала що це одночасно можна вважати перевіркою тексту, але потім прочитала що необзідно використовувати cy.get(element).should('have.text', 'your text') і додала це. 
+// тож питання в тому, чи можна су.contains уже вважати перевіркою тексту і виключити .should('have.text', 'your text')?
