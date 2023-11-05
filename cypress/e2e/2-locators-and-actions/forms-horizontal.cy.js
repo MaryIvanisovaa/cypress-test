31c64cc03b10fcc5121b364f4d1a5354337a5206
MaryIvanisovaa <ivanisovamaria07@gmail.com>
2023-11-05 16:19:19 +0200

    there are some changes related  to your comments

--- cypress/e2e/2-locators-and-actions/forms-horizontal.cy.js
+++ cypress/e2e/2-locators-and-actions/forms-horizontal.cy.js
@@ -1,7 +1,7 @@
-describe('__', ()=>{
-    context('__', ()=>{
+describe('This test checks login in Horizontal form', ()=>{
+    context('Check that all fields can be fill and can click button "Login"', ()=>{
 
-it ('__', ()=>{
+it ('All fields are available and button "Login" is clickable', ()=>{
 cy.visit('pages/forms/layouts');
 cy.get('#inputEmail3').clear().type('test123@test.com')
 cy.get('#inputPassword3').clear().type('125wrhrejetj', {log:false})
